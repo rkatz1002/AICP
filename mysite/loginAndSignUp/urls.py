595fbf04a367c7f60e51ed8b5bebaa388b209e3c
Matheus Silva Martins Mota <matheussilvamartins1714@gmail.com>
2019-08-15 00:44:54 -0300

    Conserto do logout

--- mysite/loginAndSignUp/urls.py
+++ mysite/loginAndSignUp/urls.py
@@ -1,10 +1,11 @@
-from django.urls import *
+from django.urls import path, re_path
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # path('seInscreva', views.seInscreva, name = 'seInscreva'),
-    # re_path('entrar', views.entrar, name = 'entrar'),
+    path('seInscreva', views.seInscreva, name = 'seInscreva'),
+    re_path('entrar', views.entrar, name = 'entrar'),
     path('base', views.base, name = 'base'),
+    path('logout', views.logout, name = 'logout'),
     # re_path('entrar', auth_views.LoginView.as_view(template_name='login.html'), name = 'entrar'),
 ]
